bff2c243a8f21b0c9a66fda5640afda99c647dff
O'tkir <keldibekovotkir767@gmail.com>
2026-02-03 15:27:10 +0500

    add isMember

--- src/telegram-user/telegram-user.module.ts
+++ src/telegram-user/telegram-user.module.ts
@@ -3,10 +3,12 @@ import { TelegramUserService } from './telegram-user.service';
 import { TelegramUserController } from './telegram-user.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { TelegramBotModule } from 'src/telegram-bot/telegram-bot.module';
 
 @Module({
-  imports: [],
+  imports: [TelegramBotModule],
   controllers: [TelegramUserController],
   providers: [TelegramUserService, PrismaService],
+
 })
 export class TelegramUserModule {}
